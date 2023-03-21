8b57630f7f042413905838c64e610819a8463c09
Teckturtle <elyseeumukunzi@gmail.com>
2023-03-22 01:32:14 +0200

    initial commit

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+session_unset();
+session_destroy();
+header('location:index.php');
+
+?><!--  Orginal Author Name: Mayuri K. 
+ for any PHP, Codeignitor, Laravel OR Python work contact me at mayuri.infospace@gmail.com  
+ Visit website : www.mayurik.com -->  
